5cd2e88dd5f271ee81203e81b381a8f4593db02f
shukawam <shukawam@gmail.com>
2025-01-21 15:22:40 +0900

    Fixed statements of iam policy

--- oracle-database/ai-vector-search/106-23ai-langchain-rag/iam.tf
+++ oracle-database/ai-vector-search/106-23ai-langchain-rag/iam.tf
@@ -9,5 +9,5 @@ resource "oci_identity_policy" "ocitutorial_policy" {
   compartment_id = var.compartment_ocid
   description    = "IAM Policy for OCI Tutorials"
   name           = "ocitutorials-iam-policy"
-  statements     = format("Allow dynamic-group %s to manage all-resources in tenancy", oci_identity_dynamic_group.ocitutorial_dynamic_group.name)
+  statements     = [format("Allow dynamic-group %s to manage all-resources in tenancy", oci_identity_dynamic_group.ocitutorial_dynamic_group.name)]
 }
