e277e73ba67b155f471dc0656e5c561368504de2
shukawam <shukawam@gmail.com>
2025-02-05 15:16:35 +0900

    Change default storage size in gb

--- oracle-database/ai-vector-search/106-23ai-langchain-rag/adb.tf
+++ oracle-database/ai-vector-search/106-23ai-langchain-rag/adb.tf
@@ -9,11 +9,12 @@ resource "oci_database_autonomous_database" "adp" {
   compute_model                       = "ECPU"
   is_auto_scaling_enabled             = "true"
   data_storage_size_in_tbs            = "1"
+  data_storage_size_in_gb             = "50"
   is_auto_scaling_for_storage_enabled = "false"
   admin_password                      = local.adb_admin_password
-  freeform_tags = local.freeform_tags
-  subnet_id                   = oci_core_subnet.public_subnet.id
-  is_mtls_connection_required = false
+  freeform_tags                       = local.freeform_tags
+  subnet_id                           = oci_core_subnet.public_subnet.id
+  is_mtls_connection_required         = false
   whitelisted_ips = [
     oci_core_instance.instance[count.index].public_ip
   ]
